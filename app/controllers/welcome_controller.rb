9119587021dfb5417927a7fc7ddd531e95813f57
Shohei Aoki <shoaok@gmail.com>
2017-12-09 17:29:19 +0900

    add file upload

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,21 @@
 class WelcomeController < ApplicationController
-  def index
-  end
+	def index
+	end
+
+	def upload_csv
+		uploaded_file = fileupload_param[:file]
+		output_path = Rails.root.join('public', uploaded_file.original_filename)
+
+		File.open(output_path, 'w+b') do |fp|
+			fp.write  uploaded_file.read
+		end
+
+		redirect_to action: 'index'
+	end
+
+	private
+	def fileupload_param
+	  params.require(:fileupload).permit(:file)
+	end
+
 end
