7449c0c149e3ab039a73d581873bb523bae6dbd8
Shohei Aoki <shoaok@gmail.com>
2017-12-12 09:41:28 +0900

    show csv data with ajax

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -7,9 +7,21 @@ class WelcomeController < ApplicationController
 	def upload_csv
 		uploaded_file = fileupload_param[:file]
 		read_csv = uploaded_file.read.split("\r\n").slice(5..-1).join("\r\n")
-		csv_data = CSV.parse(read_csv, headers:true)
+		@csv_data = CSV.parse(read_csv, headers:true)
 
-		logger.debug csv_data
+		logger.debug @csv_data
+		render
+		# respond_to do |format|
+		# 	format.js {
+		# 		render :template => "welcome/index.js.erb"
+		# 	}
+		# end
+
+		# respond_to do |format|
+		# 	format.js 
+		# end
+
+		# render 'index', :format => :js
 	end
 
 	private
