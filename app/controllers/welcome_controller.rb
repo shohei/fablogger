47fa0837b0215cc0d27efbeaf8ea728da43742c6
Shohei Aoki <shoaok@gmail.com>
2017-12-12 10:09:08 +0900

    clean

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -11,17 +11,6 @@ class WelcomeController < ApplicationController
 
 		logger.debug @csv_data
 		render
-		# respond_to do |format|
-		# 	format.js {
-		# 		render :template => "welcome/index.js.erb"
-		# 	}
-		# end
-
-		# respond_to do |format|
-		# 	format.js 
-		# end
-
-		# render 'index', :format => :js
 	end
 
 	private
