6237a21aae26e1611cf4aa8a1773486fe71c0a09
Shohei Aoki <shoaok@gmail.com>
2017-12-12 11:17:04 +0900

    show graph by the csv uploaded

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,6 @@
 class WelcomeController < ApplicationController
 	require 'csv'
+	MAXNUM = 1000
 
 	def index
 	end
@@ -9,7 +10,14 @@ class WelcomeController < ApplicationController
 		read_csv = uploaded_file.read.split("\r\n").slice(5..-1).join("\r\n")
 		@csv_data = CSV.parse(read_csv, headers:true)
 
-		logger.debug @csv_data
+		@temperature = @csv_data[" Temperature"].map{|a| a.to_f}.unshift("Temperature").slice(0..MAXNUM)
+		@lux = @csv_data[" Lux"].map{|a| a.to_f}.unshift("Lux").slice(0..MAXNUM)
+		@humidity = @csv_data[" Humidity"].map{|a| a.to_f}.unshift("Humidity").slice(0..MAXNUM)
+		@acc_x = @csv_data[" Acc X"].map{|a| a.to_f}.unshift("Acc X").slice(0..MAXNUM)
+		@acc_y = @csv_data[" Acc Y"].map{|a| a.to_f}.unshift("Acc Y").slice(0..MAXNUM)
+		@acc_z = @csv_data[" Acc Z"].map{|a| a.to_f}.unshift("Acc Z").slice(0..MAXNUM)
+		#binding.pry
+		#logger.debug @csv_data
 		render
 	end
 
